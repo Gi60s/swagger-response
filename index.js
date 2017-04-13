65715a0c3ae9e2c3f25dbaafac1c475c29ff431d
James <james.speirs@gmail.com>
2017-04-13 16:01:40 -0600

    move swagger response to index

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 'use strict';
 
-const SwaggerResponse   = require('./bin/swagger-response');
-SwaggerResponse.is      = require('./bin/is');
-SwaggerResponse.to      = require('./bin/convert-to');
+const enforcer          = require('./bin/enforcer');
 
 module.exports = SwaggerResponse;
+
+function SwaggerResponse(schema, options, initial) {
+    return enforcer(schema, options || {}, initial);
+}
+
+SwaggerResponse.is      = require('./bin/is');
+SwaggerResponse.to      = require('./bin/convert-to');
